cb42831c3db91673be12c27757cb9f243f39d6de
Alberto Diaz <adiaz@webdoxclm.com>
2025-06-06 19:32:20 -0400

    feat(select): implement SelectUserWorkspaceForm component for workspace selection

---
+++ src/pages/select-workspace/components/select-user-workspace-form.tsx
@@ -0,0 +1,65 @@
+import { z as zod } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { SelectControlContainer } from '@/components/form/select/select-control.container';
+
+import type { UserWorkspace } from '@/interfaces/user.interfaces';
+import { Form } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { InputControlContainer } from '@/components/form/input/input-control.container';
+
+interface FormValues {
+  email: string;
+  workspaceId: string;
+}
+
+interface SelectUserWorkspaceFormProps {
+  userWorkspaces: UserWorkspace[];
+  email: string;
+  isLoading?: boolean;
+  onSubmit: (values: FormValues) => void;
+  onExit: () => void;
+}
+
+const schema = zod.object({
+  email: zod.string().email('El correo electrónico no es válido'),
+  workspaceId: zod.string().min(1, 'Debes seleccionar un espacio de trabajo'),
+});
+
+export const SelectUserWorkspaceForm = ({ userWorkspaces, email, isLoading, onSubmit, onExit }: SelectUserWorkspaceFormProps) => {
+  const methods = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email,
+    },
+  });
+
+  return (
+    <Form {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} noValidate>
+        <Card className="w-sm">
+          <CardHeader>
+            <CardTitle>Selecciona tu espacio de trabajo</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <InputControlContainer disabled label="correo electrónico" name="email" />
+            <SelectControlContainer
+              name="workspaceId"
+              label="Espacio de trabajo"
+              options={userWorkspaces.map(({ id, name }) => ({
+                value: String(id),
+                label: name,
+              }))}
+            />
+          </CardContent>
+          <CardFooter className="flex justify-between space-x-2">
+            <Button type="submit" disabled={isLoading}>Entrar</Button>
+            <Button variant="ghost" type="button" onClick={onExit}>Salir</Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  );
+};
--- src/pages/select-workspace/containers/select-workspace.container.tsx
+++ src/pages/select-workspace/containers/select-workspace.container.tsx
@@ -1,7 +1,12 @@
+import { useAuthContext } from '@/shared/hooks/use-auth-context';
+import { SelectUserWorkspaceForm } from '../components/select-user-workspace-form';
+
 export const SelectWorkspaceContainer = () => {
+  const { user: { userWorkspaces = [], email = '' } = {} } = useAuthContext();
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-      <p className='text-lg font-semibold mb-4'>Selecciona tu espacio de trabajo</p>
+      <SelectUserWorkspaceForm email={email} userWorkspaces={userWorkspaces} onSubmit={console.log} onExit={console.log} />
     </div>
   );
 };
