6169f5917d5993862b3d5fd0a907f483a9c45588
Alberto Diaz <adiaz@webdoxclm.com>
2025-06-07 13:02:37 -0400

    refactor(layout): add background color to RootLayout for improved aesthetics

--- src/components/layouts/root/root.layout.tsx
+++ src/components/layouts/root/root.layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 export const RootLayout = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <Outlet />
     </div>
   );
